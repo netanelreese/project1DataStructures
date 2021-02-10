e1318fd60aa7e639fb63ef9776356b41fb2b6b62
Nathanael <ngreese7@ou.edu>
2021-02-10 00:26:56 -0600

    Need to fix matrix multiply

--- main.cpp
+++ main.cpp
@@ -46,9 +46,9 @@ CSR:: CSR(CSR& matrixB) { //taking in the matrix using a pointer
     colPos = new int [matrixB.nonZeros];
     rowPtr = new int [n];
 
-    for (int i = 0; i < sizeof values; ++i) this->values[i] = matrixB.values[i]; // assigning the input "values" value to the objects values value //deep copy
-    for (int i = 0; i < sizeof values; ++i) this->rowPtr[i] = matrixB.rowPtr[i]; // assigning the input "rowPtr" value to the objects rowPtr value
-    for (int i = 0; i < sizeof values; ++i) this->colPos[i] = matrixB.colPos[i]; // assigning the input "colPos" value to the objects colPos value
+    for (int i = 0; i < sizeof values; ++i) values[i] = matrixB.values[i]; // assigning the input "values" value to the objects values value //deep copy
+    for (int i = 0; i < sizeof values; ++i) rowPtr[i] = matrixB.rowPtr[i]; // assigning the input "rowPtr" value to the objects rowPtr value
+    for (int i = 0; i < sizeof values; ++i) colPos[i] = matrixB.colPos[i]; // assigning the input "colPos" value to the objects colPos value
 }
 CSR::CSR (int rows, int cols, int numNonZeros) { //assigning each of the inputs to their respective members
     n = rows;
